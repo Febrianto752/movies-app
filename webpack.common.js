78ea05d2d013a03a1cc56876a70bb22064ed447c
Febrianto752 <febrianto.bekasi@gmail.com>
2021-10-24 20:17:57 +0700

    initial commit, proccess: build carousel

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    assetModuleFilename: 'images/[hash:6][ext][query]',
+  },
+  module: {
+    rules: [{
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      // css and sass loader
+      {
+        test: /\.(sc|c)ss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '',
+          },
+        }, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+
+  ],
+
+};
